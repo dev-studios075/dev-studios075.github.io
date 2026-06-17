5a6481e31d73090fb4ae1469dc359aea18afc3ec
rk-fleetcodes <rahul@fleetcodes.com>
2026-06-17 17:07:02 +0530

    Fix GA initial pageview tracking

--- src/components/analytics/GoogleAnalytics.tsx
+++ src/components/analytics/GoogleAnalytics.tsx
@@ -1,11 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { trackPageView } from "@/lib/analytics";
 
 const GoogleAnalytics = () => {
   const location = useLocation();
+  const hasSeenInitialPage = useRef(false);
 
   useEffect(() => {
+    if (!hasSeenInitialPage.current) {
+      hasSeenInitialPage.current = true;
+      return;
+    }
+
     trackPageView(`${location.pathname}${location.search}`, document.title);
   }, [location.pathname, location.search]);
 
